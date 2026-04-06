687515609eabc1a233fab05bb993a18c8868b512
Avrora <smoyanavrora2008@gmail.com>
2026-04-06 16:17:43 +0300

    list actions correction

--- js/currencies.js
+++ js/currencies.js
@@ -1,12 +1,19 @@
-const changeCurrency = document.getElementById('change__currency');
+const changeFrom = document.getElementById('change__from');
+const changeTo = document.getElementById('change__to');
 
 export function createOption(currencyArray){
 
     currencyArray.forEach( item => {
-          const opt =  document.createElement('option');
-          opt.textContent = item[1];
-          opt.setAttribute('data-code', item[0]);
-          changeCurrency.append(opt)
+          const optFrom =  document.createElement('option');
+          const optTo =  document.createElement('option');
+          optFrom.textContent = item[1];
+          optTo.textContent = item[1];
+          optFrom.setAttribute('data-code', item[0]);
+          optTo.setAttribute('data-code', item[0]);
+          optFrom.setAttribute('class', 'from');
+          optTo.setAttribute('class', 'to');
+          changeFrom.append(optFrom);
+          changeTo.append(optTo);
     });
 
 }
@@ -18,4 +25,17 @@ fetch("https://v6.exchangerate-api.com/v6/934ed8bd54e45ea69b94dc35/codes")
         return answer.json();
     })
     .then(( data)=>{ console.log(data.supported_codes); createOption(data.supported_codes)})
+
+
+const totalCurrency = document.createElement('p')
+totalCurrency.setAttribute('class', 'total__currency')
+const convertForm = document.querySelector(".currency__changer")
+// export const convertBtn = () => convertForm.addEventListener('submit', () => {
+//     const coeff = 
+
+
+//     totalCurrency.textContent = `=${} ${optTo.getAttribute('data-code')}`
+// })
+
+
 
--- js/list__actions.js
+++ js/list__actions.js
@@ -12,11 +12,11 @@ export const list__actions = () => list.addEventListener('click', (event) => {
     
 })
 
-const listItemInput = document.querySelector(".list__elem__input")
+let amount
 
 const onWhat = document.getElementById('on__what')
 const howMuch = document.getElementById('how__much')
-
+const newListItem = document.createElement('li');
 const addItemForm = document.querySelector(".add__item")
 addItemForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -26,22 +26,23 @@ addItemForm.addEventListener('submit', (event) => {
         return;
     }
 
-    const amount = parseFloat(howMuch.value);
+    amount = parseFloat(howMuch.value);
     if (isNaN(amount)) {
         alert('Сумма должна быть числом!');
         return;
     }
     
-    const newListItem = document.createElement('li');
+
     newListItem.className = 'total__list__elem';
     newListItem.innerHTML = `${onWhat.value} <div class="container"><input type="text" class="list__elem__input" value="${amount}"><button class="delete">🗑️</button></div>`;
     
+    const newInput = newListItem.querySelector('.list__elem__input');
+    newInput.addEventListener('input', () => validateInput(newInput));
     list.appendChild(newListItem);
     
     onWhat.value = '';
     howMuch.value = '';
 })
-
 
 const clearButton = document.querySelector(".clear")
 export const clearBtn = () => clearButton.addEventListener('click', () => {
